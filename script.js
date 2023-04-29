4df2d35a76a193033bf7215520b2c006bacbda17
Daiane da S. Santos <daianesadm@outlook.com>
2023-04-29 11:41:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,54 @@ const canvasElement = document.querySelector('canvas'),
 
 const lineWidth = 15
 
+// Campo
+const field = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+  draw: function () {
+    // Desenho do campo
+    canvasContext.fillStyle = '#000000'
+    canvasContext.fillRect(0, 0, this.w, this.h)
+  }
+}
+
+// Linha central do campo
+const line = {
+  w: 15,
+  h: field.h,
+  draw: function () {
+    // Desenho da linha central do campo
+    canvasContext.fillStyle = '#ffffff'
+    canvasContext.fillRect(field.w / 2 - this.w / 2, 0, this.w, this.h)
+  }
+}
+
+// Raquete da esquerda
+const leftPaddle = {
+  x: gapX,
+  y: 240,
+  w: line.w,
+  h: 100,
+  draw: function () {
+    // Desenho da raquete esquerda
+    canvasContext.fillStyle = '#ffffff'
+    canvasContext.fillRect(this.x, this.y, this.w, this.h)
+  }
+}
+
+// Raquete da direita
+const rightPaddle = {
+  x: field.w - line.w - gapX,
+  y: 240,
+  w: line.w,
+  h: 100,
+  draw: function () {
+    // Desenho da raquete direita
+    canvasContext.fillStyle = '#ffffff'
+    canvasContext.fillRect(this.x, this.w, this.w, this.h)
+  }
+}
+
 // Definir a dimensão dos elementos da tela
 function setup() {
   canvasElement.width = canvasContext.width = window.innerWidth
@@ -11,33 +59,18 @@ function setup() {
 }
 
 // Desenho dos elementos no cenário
-
-// Cor de fundo do campo
 function draw() {
-  canvasContext.fillStyle = '#000000'
-  canvasContext.fillRect(0, 0, window.innerWidth, window.innerHeight)
+  field.draw() // Campo
+  line.draw() // Linha de divisão do campo
 
-  // Linha de divisão do campo
-  canvasContext.fillStyle = '#ffffff'
-  canvasContext.fillRect(
-    window.innerWidth / 2 - lineWidth / 2,
-    0,
-    lineWidth,
-    window.innerHeight
-  )
+  leftPaddle.draw() // Raquete esquerda
+  rightPaddle.draw() // Raquete direita
 
-  // Raquete esquerda
+  // Bolinha
   canvasContext.fillStyle = '#ffffff'
-  canvasContext.fillRect(gapX, 300, lineWidth, 100)
-
-  // Raquete direita
-  canvasContext.fillStyle = '#ffffff'
-  canvasContext.fillRect(
-    window.innerWidth - lineWidth - gapX,
-    300,
-    lineWidth,
-    100
-  )
+  canvasContext.beginPath()
+  canvasContext.arc(120, 240, 10, 0, 2 * Math.PI, false)
+  canvasContext.fill()
 }
 
 setup()
